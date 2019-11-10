65159bce50ddbba1239ed7e814283c12d0b8964a
argsun <a.hadadi@gmail.com>
2019-11-10 17:52:09 +0330

    Thired10

--- Script/script9bf3.js
+++ Script/script9bf3.js
@@ -268,14 +268,14 @@ $(function() {
 
   Quagga.onDetected(function(result) {
       var code = result.codeResult.code;
-if (code.length===17 && !isNaN(code)){
+if (code.length===17 && !isNaN(code) && code.endsWith("101")){
   ScanCode=code
       if (App.lastResult !== code) {
           App.lastResult = code;
           var $node = null, canvas = Quagga.canvas.dom.image;
 
           $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-          $node.find("img").attr("src", canvas.toDataURL());
+         // $node.find("img").attr("src", canvas.toDataURL());
           $node.find("h4.code").html(code);
           $("#result_strip ul.thumbnails").prepend($node);
           var audio = new Audio("success.wav");
